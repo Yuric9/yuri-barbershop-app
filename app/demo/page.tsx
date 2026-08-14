1310d428326c7a92edb6ea0851845a2980ea0331
Yuri Cesar <yure-c@hotmail.com>
2026-08-14 15:24:28 -0300

    Ajusta experiência pública e responsividade

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,5 +1,5 @@
 import PortalClient from "../portal-client";
 
 export default function Demo() {
-  return <PortalClient user={{ name: "João Cliente", email: "cliente@exemplo.com" }} role="client" demo />;
+  return <PortalClient user={{ name: "Visitante", email: "" }} role="client" demo />;
 }
