a689f39c816af6bf25895c0c57edfdc0c97b9d5e
Yuri Cesar <yure-c@hotmail.com>
2026-08-17 14:04:34 -0300

    Fix D1 appointment slot migration for remote deploy

--- drizzle/0017_appointment_slot_lock.sql
+++ drizzle/0017_appointment_slot_lock.sql
@@ -7,21 +7,6 @@ CREATE TABLE IF NOT EXISTS appointment_slots (
   appointment_id INTEGER,
   created_at TEXT NOT NULL
 );
-CREATE UNIQUE INDEX IF NOT EXISTS appointment_slots_unique ON appointment_slots (date, time, resource_key);
 
-CREATE TRIGGER IF NOT EXISTS prevent_overlapping_appointments
-BEFORE INSERT ON appointments
-WHEN NEW.status <> 'Cancelado'
-BEGIN
-  SELECT CASE WHEN EXISTS (
-    SELECT 1
-    FROM appointments existing
-    LEFT JOIN services existing_service ON existing_service.id = existing.service_id
-    LEFT JOIN services new_service ON new_service.id = NEW.service_id
-    WHERE existing.date = NEW.date
-      AND existing.status <> 'Cancelado'
-      AND (NEW.collaborator_id IS NULL OR existing.collaborator_id IS NULL OR existing.collaborator_id = NEW.collaborator_id)
-      AND ((CAST(substr(NEW.time, 1, 2) AS INTEGER) * 60 + CAST(substr(NEW.time, 4, 2) AS INTEGER)) < (CAST(substr(existing.time, 1, 2) AS INTEGER) * 60 + CAST(substr(existing.time, 4, 2) AS INTEGER) + COALESCE(existing_service.duration_min, 30)))
-      AND ((CAST(substr(existing.time, 1, 2) AS INTEGER) * 60 + CAST(substr(existing.time, 4, 2) AS INTEGER)) < (CAST(substr(NEW.time, 1, 2) AS INTEGER) * 60 + CAST(substr(NEW.time, 4, 2) AS INTEGER) + COALESCE(new_service.duration_min, 30)))
-  ) THEN RAISE(ABORT, 'appointment_overlap') END;
-END;
+CREATE UNIQUE INDEX IF NOT EXISTS appointment_slots_unique
+ON appointment_slots (date, time, resource_key);
