387d24c5c984c68acdb74b503a36bf2c40241e46
Yuri Cesar <yure-c@hotmail.com>
2026-08-24 14:16:00 -0300

    fix: remove duplicate loyalty migration number

--- drizzle/0018_loyalty_card_admin.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `profiles` ADD `loyalty_adjustment` integer DEFAULT 0 NOT NULL;
-ALTER TABLE `profiles` ADD `loyalty_rewards_redeemed` integer DEFAULT 0 NOT NULL;
-ALTER TABLE `profiles` ADD `loyalty_adjustment_note` text DEFAULT '' NOT NULL;
-ALTER TABLE `profiles` ADD `loyalty_updated_at` text DEFAULT '' NOT NULL;
-ALTER TABLE `profiles` ADD `loyalty_updated_by` text DEFAULT '' NOT NULL;
